648df19a0fefc86b2e39da72303433868b06b6d5
Daniel Richardson <drich1475@gmail.com>
2025-03-24 00:20:07 -0400

    Initial Commit

---
+++ Myoelectric Prosthetic/Myoelectric_ProstheticApp.swift
@@ -0,0 +1,17 @@
+//
+//  Myoelectric_ProstheticApp.swift
+//  Myoelectric Prosthetic
+//
+//  Created by Daniel Richardson on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Myoelectric_ProstheticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
